8cd724ae12738905acbb6052b8649b02706c5ed6
Jeremy Chang <chang489@purdue.edu>
2021-02-03 21:01:48 -0500

    algebraic approach instead of cs approach

--- graph_hom.py
+++ graph_hom.py
@@ -1,78 +1,94 @@
 import numpy as np
+import sys
 
 def H(k, V, E, Z):
-	# map of vertex to index
-	G = {}
-	vnum = {}
-	for i, v in enumerate(V):
-		vnum[v] = i
-		G[v] = []
-
-	for (v_1, v_2) in E:
-		G[v_1].append(v_2)
-		G[v_2].append(v_1)
+	C_0 = lc(len(V))
+	C_1 = lc(len(E))
 
 	# partial function
 	partial_1 = np.zeros((len(V), len(E)))
-
 	for i, (v_1, v_2) in enumerate(E):
-		partial_1[vnum[v_1], i] = 1
-		partial_1[vnum[v_2], i] = 1
+		partial_1[V.index(v_1), i] = 1
+		partial_1[V.index(v_2), i] = 1
 
-	ans = solve(partial_1)
+	Z_0 = C_0
+	Z_1 = ker(partial_1, C_1, C_0)
+
+	B_0 = img(partial_1, C_1, C_0)
+	B_1 = []
 
 	if k == 0:
-		# equivalence classes
-		equiv = []
-		equiv.append(set('0'))
+		equiv = {}
+		for i in range(len(Z_0)):
+			for j in range(i+1, len(Z_0)):
+				b = (Z_0[j] - Z_0[i]) % 2
+				if np.any(np.array_equal(b, x) for x in B_0):
+					if np.sum(Z_0[i]) == 1 and np.sum(Z_0[j]) == 1:
+						if V[np.argmax(Z_0[i])] not in equiv:
+							equiv[V[np.argmax(Z_0[i])]] = []
+						if V[np.argmax(Z_0[j])] not in equiv:
+							equiv[V[np.argmax(Z_0[j])]] = []
+						equiv[V[np.argmax(Z_0[i])]].append(V[np.argmax(Z_0[j])])
+						equiv[V[np.argmax(Z_0[j])]].append(V[np.argmax(Z_0[i])])
 
-		# find vertices that are equivalent to 0
-		for a in ans:
-			if np.count_nonzero(a) == 1:
-				equiv[0].add(V[np.argmax(a > 0)])
-
-		# do the rest
-		visited = []
+		ZqB = [[0]]
+		vis = set()
 		for v in V:
-			visited.append(False)
-
-		for v in V:
-			if v not in equiv[0] and not visited[vnum[v]]:
-				s = set(v)
-				dfs(G, vnum, v, visited, s)
-				equiv.append(s)
-
-		return equiv
-
+			if not (v in vis):
+				vis.add(v)
+				e = dfs(equiv, [v], vis, v)
+				if len(e) != 0:
+					ZqB.append(e)
+		return ZqB
 	if k == 1:
-		return ans
+		return Z_1
 
-def solve(arr):
-	row, col = arr.shape
+# f: A->B
+def img(f, A, B):
 	ans = []
-
-	n = 2**col
-	for i in range(n):
-		sol = np.zeros((col, 1))
-		b = bin(i)[2:]
-		for j, c in enumerate(b[::-1]):
-			if c == '0':
-				sol[col-j-1, 0] = 0
-			else:
-				sol[col-j-1, 0] = 1
-		if np.count_nonzero(np.matmul(arr, sol) % 2) == 0:
-			ans.append(sol)
+	for a in A:
+		fa = np.matmul(f, a) % 2
+		if np.any(np.array_equal(fa, x) for x in A):
+			ans.append(fa)
 	return ans
 
-def dfs(G, vnum, v, visited, s):
-	if len(G[v]) == 0:
-		return
-	for child in G[v]:
-		if not visited[vnum[child]]:
-			visited[vnum[child]] = True
-			s.add(child)
-			dfs(G, vnum, child, visited, s)
-	return
+# f: A->B
+def ker(f, A, B):
+	row, col = f.shape
+	sol = lc(col)
+	ans = []
+
+	for v in sol:
+		if np.count_nonzero(np.matmul(f, v) % 2) == 0:
+			ans.append(v)
+
+	return ans
+
+# all linear combinations in mod 2
+def lc(n):
+	m = 2**n
+	ans = []
+	for i in range(m):
+		b = bin(i)[2:]
+		v = np.zeros(n)
+		for j, c in enumerate(b[::-1]):
+			if c == '0':
+				v[n-j-1] = 0
+			else:
+				v[n-j-1] = 1
+		ans.append(v)
+	return ans
+
+def dfs(equiv, e, vis, v):
+	if len(equiv[v]) == 0:
+		return e
+	for w in equiv[v]:
+		if w not in vis:
+			e.append(w)
+			vis.add(w)
+			dfs(equiv, e, vis, w)
+
+	return e
 
 def run(n):
 	if n == 1.6:
@@ -87,7 +103,11 @@ def run(n):
 	elif n == 1.9:
 		V = ['a','b','c','d']
 		E = [('a','b'), ('b','c'), ('b','d')]
+	elif n == 0.1:
+		V = ['a','b','c','d']
+		E = [('a', 'b'), ('a','c'), ('b','c'), ('b','d'), ('c','d')]
 	else:
+		n = int(n)
 		V = []
 		for i in range(n):
 			V.append(str(i))
@@ -95,11 +115,12 @@ def run(n):
 		for i in range(n):
 			for j in range(i+1, n):
 				E.append((V[i], V[j]))
-		print(V)
-		print(E)
 
 	print(H(0, V, E, 2))
 	print(H(1, V, E, 2))
 
 if __name__ == '__main__':
-	run(5)
+	if len(sys.argv) != 2:
+		print('Usage: python graph_hom.py <exercise/n>')
+	else:
+		run(float(sys.argv[1]))
